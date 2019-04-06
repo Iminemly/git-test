784c315e923f4fc04d6420ce014cafb7bfc06ebe
jiangfuxi <jiangfuxi@163.com>
2019-04-06 11:23:52 +0800

    将本地工程添加到git中

---
+++ git_test_project/src/com/iminemly/git/FirstClass.java
@@ -0,0 +1,28 @@
+package com.iminemly.git;
+
+/**
+ * @program: git_test_project
+ * * @author: Mr.jiang
+ * * @create: 2019-04-05 22:08
+ **/
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
